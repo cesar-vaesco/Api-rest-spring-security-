3afc82dee6d2b4a82eccd175e1b6925279752ca1
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-12-13 20:10:40 -0600

    Api-Rest: Entity(Profesor)

---
+++ src/main/java/com/vaescode/apirest/restservice/entity/Profesor.java
@@ -0,0 +1,94 @@
+package com.vaescode.apirest.restservice.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity(name = "profesores")
+public class Profesor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(length = 60, unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(length = 2000)
+    private String foto;
+
+    @Column(name="create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFoto() {
+        return this.foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Date getCreateAt() {
+        return this.createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+
+    private static final long serialVersionUID = 1L;
+
+}
